28427d9c2345256b345aa9ec826c87c16275f6d2
Anton <antoncodes@outlook.com>
2024-05-09 21:59:02 +0300

    CheckboxList

--- app/routes/_main._index.tsx
+++ app/routes/_main._index.tsx
@@ -1,6 +1,20 @@
-import { Container, Typography } from '@mui/material';
+import { Delete, Edit } from '@mui/icons-material';
+import {
+  Checkbox,
+  Container,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Stack,
+  Typography,
+} from '@mui/material';
 import type { MetaFunction } from '@remix-run/node';
+import { useState } from 'react';
 
+// meta
 export const meta: MetaFunction = () => {
   return [
     { title: 'Remix Todo' },
@@ -8,12 +22,76 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export default function Index() {
+// sub-component
+function CheckboxList() {
+  const [checked, setChecked] = useState([0]);
+
+  const handleToggle = (value: number) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+  return (
+    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      {[0, 1, 2, 3].map((value) => {
+        const labelId = `checkbox-list-label-${value}`;
+
+        return (
+          <ListItem
+            key={value}
+            secondaryAction={
+              <>
+                <IconButton size="small" aria-label="comments">
+                  <Edit fontSize="small" />
+                </IconButton>
+                <IconButton edge="end" size="small" aria-label="comments">
+                  <Delete fontSize="small" />
+                </IconButton>
+              </>
+            }
+            disablePadding
+          >
+            <ListItemButton
+              role={undefined}
+              onClick={handleToggle(value)}
+              dense
+            >
+              <ListItemIcon>
+                <Checkbox
+                  edge="start"
+                  checked={checked.indexOf(value) !== -1}
+                  tabIndex={-1}
+                  disableRipple
+                  inputProps={{ 'aria-labelledby': labelId }}
+                />
+              </ListItemIcon>
+              <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+            </ListItemButton>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+}
+
+/**
+ * Index (root) route component.
+ */
+export default function RouteIndex() {
   return (
     <Container>
       <Typography variant="h4" component="h1" mb={3}>
         My Todos
       </Typography>
+      <CheckboxList />
     </Container>
   );
 }
