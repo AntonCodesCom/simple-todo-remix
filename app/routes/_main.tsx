ea25cb78a79ae2fe24da54a027ca70c3ec1dfcd5
Anton <antoncodes@outlook.com>
2024-05-10 18:22:55 +0300

    session id via search params

--- app/routes/_main.tsx
+++ app/routes/_main.tsx
@@ -1,6 +1,7 @@
 import { LoaderFunctionArgs, json } from '@remix-run/node';
 import { Outlet, useLoaderData } from '@remix-run/react';
 import { v4 } from 'uuid';
+import validator from 'validator';
 import CommonLayout from '~/Common/components/Layout';
 import CommonSession from '~/Common/components/Session';
 import sessions from '~/sessions';
@@ -9,8 +10,24 @@ import sessions from '~/sessions';
 export async function loader({ request }: LoaderFunctionArgs) {
   const { getSession, commitSession, sessionCookieName } = sessions();
   const session = await getSession(request.headers.get('Cookie'));
+  const url = new URL(request.url);
+  const sessionIdParam = url.searchParams.get('sessionId');
+  if (sessionIdParam && validator.isUUID(sessionIdParam)) {
+    // if session ID is set via search params
+    // then overriding existing session ID if any
+    session.set(sessionCookieName, sessionIdParam);
+    return json(
+      { sessionId: sessionIdParam },
+      {
+        headers: {
+          'Set-Cookie': await commitSession(session),
+        },
+      },
+    );
+  }
   let sessionId = session.get(sessionCookieName) as string;
   if (sessionId) {
+    // if session ID has already been set
     return json({ sessionId });
   }
   sessionId = v4();
