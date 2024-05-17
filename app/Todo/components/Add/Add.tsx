d979a40dc6dfd475f3647c8c4b947f0fb224f83a
Anton <antoncodes@outlook.com>
2024-05-17 22:18:12 +0300

    clearing label field after submit success

--- app/Todo/components/Add/Add.tsx
+++ app/Todo/components/Add/Add.tsx
@@ -1,10 +1,20 @@
 import { Add } from '@mui/icons-material';
 import { Button, Stack, TextField } from '@mui/material';
 import { useFetcher } from '@remix-run/react';
+import { useEffect, useState } from 'react';
 
 export default function TodoAdd() {
   const fetcher = useFetcher();
+  const [label, setLabel] = useState('');
+
   // TODO: loading state
+
+  useEffect(() => {
+    if (fetcher.state === 'idle') {
+      setLabel('');
+    }
+  }, [fetcher.state]);
+
   return (
     <fetcher.Form method="POST" action="add">
       <Stack direction="row" gap={0.5}>
@@ -13,6 +23,8 @@ export default function TodoAdd() {
           placeholder="Something to do..."
           size="small"
           required
+          value={label}
+          onChange={(e) => setLabel(e.target.value)}
         />
         <Button type="submit" variant="contained" startIcon={<Add />}>
           Add
