0cf2e151ec0cea68936c98175823a9280f2a110e
Anton <antoncodes@outlook.com>
2024-05-22 19:42:09 +0300

    deletion popover

--- app/Todo/components/List/Item.tsx
+++ app/Todo/components/List/Item.tsx
@@ -1,7 +1,15 @@
-import { Box, Checkbox, IconButton, Stack, Typography } from '@mui/material';
-import { Delete, Edit } from '@mui/icons-material';
+import {
+  Box,
+  Button,
+  Checkbox,
+  IconButton,
+  Popover,
+  Stack,
+  Typography,
+} from '@mui/material';
+import { Block, Delete, DeleteOutlined, Edit } from '@mui/icons-material';
 import TodoItem from '~/Todo/types/Item';
-import { useState } from 'react';
+import { MouseEvent, useState } from 'react';
 import styles from './Item.module.css';
 
 interface Props {
@@ -10,8 +18,18 @@ interface Props {
 
 export default function TodoListItem({ todo }: Props) {
   const { id, label, done } = todo;
-  const htmlId = `TodoListItem_${id}`;
+  const checkboxHtmlId = `TodoListItem_checkbox-${id}`;
+  const popoverHtmlId = `TodoListItem_popover-${id}`;
   const [editingActive, setEditingActive] = useState(false);
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+  const popoverOpen = Boolean(anchorEl);
+
+  function handlePopoverOpen(e: MouseEvent<HTMLButtonElement>) {
+    setAnchorEl(e.currentTarget);
+  }
+  function handlePopoverClose() {
+    setAnchorEl(null);
+  }
 
   return (
     <Box role="listitem" className={styles.root}>
@@ -29,12 +47,12 @@ export default function TodoListItem({ todo }: Props) {
           component="form"
           onSubmit={(e) => e.preventDefault()}
         >
-          <Checkbox id={htmlId} defaultChecked={done} />
+          <Checkbox id={checkboxHtmlId} defaultChecked={done} />
           <Box flex={1} pr={1}>
             <Typography
               variant="body2"
               component="label"
-              htmlFor={htmlId}
+              htmlFor={checkboxHtmlId}
               sx={{
                 display: 'block',
                 cursor: 'pointer',
@@ -49,11 +67,49 @@ export default function TodoListItem({ todo }: Props) {
             <Edit fontSize="small" />
           </IconButton>
           <Box pl={0.25} />
-          <IconButton size="small">
+          <IconButton size="small" onClick={handlePopoverOpen}>
             <Delete fontSize="small" />
           </IconButton>
         </Stack>
       </Box>
+      <Popover
+        id={popoverHtmlId}
+        anchorEl={anchorEl}
+        open={popoverOpen}
+        onClose={handlePopoverClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+      >
+        <Box maxWidth="20rem" p={1}>
+          <Typography variant="body2" textAlign="center" mb={1}>
+            Are you sure?
+          </Typography>
+          <Stack direction="row" gap={0.5}>
+            <Button
+              variant="outlined"
+              size="small"
+              color="error"
+              startIcon={<DeleteOutlined />}
+              onClick={() => {
+                handlePopoverClose();
+                // TODO: deletion
+              }}
+            >
+              Yes
+            </Button>
+            <Button
+              variant="contained"
+              size="small"
+              startIcon={<Block />}
+              onClick={handlePopoverClose}
+            >
+              No
+            </Button>
+          </Stack>
+        </Box>
+      </Popover>
     </Box>
   );
 }
