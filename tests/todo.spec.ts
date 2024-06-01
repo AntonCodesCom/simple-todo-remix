92254ececc0d9553ce6c2546eaf583fc5b03713f
Anton <antoncodes@outlook.com>
2024-06-01 16:47:37 +0300

    e2e comments

--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -18,11 +18,21 @@ async function getSessionCookie(userId: string) {
 // e2e test
 //
 test('Todo', async ({ page }) => {
+  // defining data
   const url = 'http://localhost:5173/'; // TODO: env
   const aliceUserId = '878664be-1926-44ab-9c77-eb5d803369be'; // fixture
+
+  // TODO: data seeding
+  // TODO: GET /todo backend request
+
+  // setting user session (via cookies)
   const sessionCookie = await getSessionCookie(aliceUserId);
   await page.context().addCookies([{ ...sessionCookie, url }]);
+
+  // visiting the page
   await page.goto(url);
+
+  // asserting todo list is on the page
   const list = page.getByRole('list', { name: 'My Todos' });
   await expect(list).toBeVisible();
 });
