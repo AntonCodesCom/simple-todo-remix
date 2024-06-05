1f18b98423f1241677fcc879ce591030d0ce1435
Anton <antoncodes@outlook.com>
2024-06-05 20:44:13 +0300

    uninstalling jest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  rootDir: './app',
-  moduleNameMapper: {
-    '^~(.*)$': '<rootDir>$1',
-  },
-};
