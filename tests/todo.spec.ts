6e98b7ef9a261130bc2ad1a9474158e621075d92
Anton <antoncodes@outlook.com>
2024-06-15 14:48:19 +0300

    splitting e2e test

--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -1,8 +1,16 @@
-import test, { expect } from '@playwright/test';
+import test, {
+  APIRequestContext,
+  Locator,
+  Page,
+  expect,
+} from '@playwright/test';
 import sessions from '../app/sessions';
 import config from '~/config';
 import arrayIdHash from '~/Common/utils/arrayIdHash';
 import { faker } from '@faker-js/faker';
+
+// config
+test.describe.configure({ mode: 'serial' });
 
 // utility
 async function generateSessionCookie(userId: string) {
@@ -17,132 +25,224 @@ async function generateSessionCookie(userId: string) {
   };
 }
 
-//
-// e2e test
-//
-test('Todo', async ({ page, request }) => {
-  // defining data
-  const { baseUrl, apiBaseUrl } = config();
-  const aliceUserId = '878664be-1926-44ab-9c77-eb5d803369be'; // fixture
+// utility
+function getTodoList(page: Page): Locator {
+  return page.getByRole('list', { name: 'My Todos' });
+}
 
-  // data seeding
-  const seedUrl = new URL('seed', apiBaseUrl).toString();
-  await request.fetch(seedUrl, { method: 'POST', failOnStatusCode: true });
-
+// utility
+async function fetchBackendTodos(
+  request: APIRequestContext,
+  apiBaseUrl: string,
+  accessToken: string,
+): Promise<any[]> {
   // requesting control data
   const getTodoUrl = new URL('todo', apiBaseUrl).toString();
   const res = await request.fetch(getTodoUrl, {
     headers: {
-      Authorization: `Bearer ${aliceUserId}`,
+      Authorization: `Bearer ${accessToken}`,
     },
   });
-  const controlTodos = await res.json();
-  expect(controlTodos.length).toBeGreaterThanOrEqual(3);
+  return (await res.json()) as any[];
+}
 
-  // setting user session (via cookies)
-  const sessionCookie = await generateSessionCookie(aliceUserId);
-  await page.context().addCookies([{ ...sessionCookie, url: baseUrl }]);
+//
+// e2e test
+//
+test.describe('Todo', () => {
+  // values used throughout the test suite
+  const { baseUrl, apiBaseUrl } = config();
+  const aliceUserId = '878664be-1926-44ab-9c77-eb5d803369be'; // fixture
+  const accessToken = aliceUserId; // TODO: update when new auth system is introduced
 
-  // visiting the page
-  await page.goto('/');
-
-  // DISPLAYING TODOS
-  // asserting Todo list contains correct Todos
-  const list = page.getByRole('list', { name: 'My Todos' });
-  await expect(list).toBeVisible();
-  const controlIdHash = arrayIdHash(controlTodos);
-  await expect(list).toHaveAttribute('data-idhash', controlIdHash);
-
-  // ADDING A TODO
-  const addedTodoLabel = 'E2E added Todo label.';
-  const addTodoForm = page.getByRole('form', { name: 'Add Todo' });
-  await expect(addTodoForm).toBeVisible();
-  const addTodoFormInput = addTodoForm.getByRole('textbox', {
-    name: 'Something to do...',
-  });
-  await expect(addTodoFormInput).toBeVisible();
-  await addTodoFormInput.fill(addedTodoLabel);
-  await addTodoForm.dispatchEvent('submit');
-  const addedTodoCard = list.getByRole('listitem', { name: addedTodoLabel });
-  await expect(addedTodoCard).toBeVisible();
-  const addedTodoCardCheckbox = addedTodoCard.getByRole('checkbox', {
-    name: addedTodoLabel,
-  });
-  await expect(addedTodoCardCheckbox).toBeChecked({ checked: false });
-
-  // to be used in further testing
-  const listitems = await list
-    .getByRole('listitem')
-    .filter({ hasNotText: addedTodoLabel })
-    .all();
-  // const pickedListItems = faker.helpers.arrayElements(listitems, 3);
-  const pickedListItems = listitems;
-
-  // TOGGLING A TODO
-  const todoToToggle = pickedListItems[0];
-  const todoToToggleId = await todoToToggle.getAttribute('id');
-  const todoToToggleCheckbox = todoToToggle.getByRole('checkbox');
-  const todoToToggleInitiallyChecked = await todoToToggleCheckbox.isChecked();
-  const todoToToggleExpectedChecked = !todoToToggleInitiallyChecked;
-  await todoToToggleCheckbox.click(); // TODO: "change" event
-  await expect(todoToToggleCheckbox).toBeChecked({
-    checked: todoToToggleExpectedChecked,
+  // before each
+  test.beforeEach(async ({ page, request }) => {
+    // data seeding
+    const seedUrl = new URL('seed', apiBaseUrl).toString();
+    await request.fetch(seedUrl, { method: 'POST', failOnStatusCode: true });
+    // setting user session (via cookies)
+    const sessionCookie = await generateSessionCookie(accessToken);
+    await page.context().addCookies([{ ...sessionCookie, url: baseUrl }]);
+    // visiting the page
+    await page.goto('/');
   });
 
-  // EDITING A TODO
-  const todoToEdit = pickedListItems[1];
-  const todoToEditId = await todoToEdit.getAttribute('id');
-  const editButton = todoToEdit.getByRole('button', { name: 'Edit' });
-  await expect(editButton).toBeVisible();
-  await editButton.click();
-  const editForm = todoToEdit.getByRole('form', { name: 'Edit Todo' });
-  await expect(editForm).toBeVisible();
-  const editFormInput = editForm.getByRole('textbox', {
-    name: 'Something to do...',
+  // displaying
+  test('displaying', async ({ page, request }) => {
+    // requesting control data
+    const backendTodos = await fetchBackendTodos(
+      request,
+      apiBaseUrl,
+      accessToken,
+    );
+    expect(backendTodos.length).toBeGreaterThanOrEqual(1);
+    // asserting the Todo list contains correct Todos via a checksum
+    const list = getTodoList(page);
+    await expect(list).toBeVisible();
+    const expectedIdHash = arrayIdHash(backendTodos);
+    await expect(list).toHaveAttribute('data-idhash', expectedIdHash);
   });
-  await expect(editFormInput).toBeVisible();
-  const editedTodoLabel = 'E2E edited Todo label.';
-  await editFormInput.fill(editedTodoLabel);
-  await editForm.dispatchEvent('submit');
-  const editedTodo = list.getByRole('listitem', { name: editedTodoLabel });
-  await expect(editedTodo).toBeVisible();
-  // asserting `editedTodo` and `todoToEdit` are the same element
-  expect(await editedTodo.getAttribute('id')).toEqual(todoToEditId);
 
-  // DELETING A TODO
-  const todoToDelete = pickedListItems[2];
-  const todoToDeleteId = await todoToDelete.getAttribute('id');
-  const deleteButton = todoToEdit.getByRole('button', { name: 'Delete' });
-  await expect(deleteButton).toBeVisible();
-  await deleteButton.click();
-  const deleteDialog = page.getByRole('dialog', { name: 'Delete this Todo?' });
-  await expect(deleteDialog).toBeVisible();
-  await deleteDialog.getByRole('button', { name: 'Yes' }).click();
-
-  // BACKEND DATA POST-VERIFICATION
-  const res2 = await request.fetch(getTodoUrl, {
-    headers: {
-      Authorization: `Bearer ${aliceUserId}`,
-    },
+  // adding
+  test('adding', async ({ page, request }) => {
+    const addedTodoLabel = faker.lorem.sentence();
+    const addTodoForm = page.getByRole('form', { name: 'Add Todo' });
+    await expect(addTodoForm).toBeVisible();
+    const addTodoFormInput = addTodoForm.getByRole('textbox', {
+      name: 'Something to do...',
+    });
+    await expect(addTodoFormInput).toBeVisible();
+    await addTodoFormInput.fill(addedTodoLabel);
+    await addTodoForm.dispatchEvent('submit');
+    const list = getTodoList(page);
+    const addedTodoCard = list.getByRole('listitem', { name: addedTodoLabel });
+    await expect(addedTodoCard).toBeVisible();
+    const addedTodoCardCheckbox = addedTodoCard.getByRole('checkbox');
+    await expect(addedTodoCardCheckbox).toBeChecked({ checked: false });
+    // asserting the added Todo exists on the backend
+    const backendTodos = await fetchBackendTodos(
+      request,
+      apiBaseUrl,
+      accessToken,
+    );
+    const backendAddedTodoIndex = backendTodos.findIndex(
+      (x: any) => x.label === addedTodoLabel,
+    );
+    expect(backendAddedTodoIndex).not.toEqual(-1);
   });
-  const controlTodos2 = await res2.json();
-  // verifying updated Todo array
-  const controlIdHash2 = arrayIdHash(controlTodos2);
-  expect(controlIdHash2).not.toEqual(controlIdHash);
-  await expect(list).toHaveAttribute('data-idhash', controlIdHash2);
-  // verifying added Todo exists
-  const controlAddedTodoIndex = controlTodos2.findIndex(
-    (x: any) => x.label === addedTodoLabel,
-  );
-  expect(controlAddedTodoIndex).not.toEqual(-1);
-  // verifying toggled Todo's "done" state is updated
-  const controlTodoToToggle = controlTodos2.find(
-    (x: any) => x.id === todoToToggleId,
-  );
-  expect(controlTodoToToggle.done).toEqual(todoToToggleExpectedChecked);
-  // verifying edited Todo has the new label
-  const controlEditedTodo = controlTodos2.find(
-    (x: any) => x.id === todoToEditId,
-  );
-  expect(controlEditedTodo.label).toEqual(editedTodoLabel);
+
+  test.skip('toggling', async () => {});
+
+  test.skip('editing', async () => {});
+
+  test.skip('deletion', async () => {});
+
+  test.skip('DEPRECATED', async ({ page, request }) => {
+    // defining data
+    const { baseUrl, apiBaseUrl } = config();
+    const aliceUserId = '878664be-1926-44ab-9c77-eb5d803369be'; // fixture
+
+    // data seeding
+    const seedUrl = new URL('seed', apiBaseUrl).toString();
+    await request.fetch(seedUrl, { method: 'POST', failOnStatusCode: true });
+
+    // requesting control data
+    const getTodoUrl = new URL('todo', apiBaseUrl).toString();
+    const res = await request.fetch(getTodoUrl, {
+      headers: {
+        Authorization: `Bearer ${aliceUserId}`,
+      },
+    });
+    const controlTodos = await res.json();
+    expect(controlTodos.length).toBeGreaterThanOrEqual(3);
+
+    // setting user session (via cookies)
+    const sessionCookie = await generateSessionCookie(aliceUserId);
+    await page.context().addCookies([{ ...sessionCookie, url: baseUrl }]);
+
+    // visiting the page
+    await page.goto('/');
+
+    // DISPLAYING TODOS
+    // asserting Todo list contains correct Todos
+    const list = page.getByRole('list', { name: 'My Todos' });
+    await expect(list).toBeVisible();
+    const controlIdHash = arrayIdHash(controlTodos);
+    await expect(list).toHaveAttribute('data-idhash', controlIdHash);
+
+    // ADDING A TODO
+    const addedTodoLabel = 'E2E added Todo label.';
+    const addTodoForm = page.getByRole('form', { name: 'Add Todo' });
+    await expect(addTodoForm).toBeVisible();
+    const addTodoFormInput = addTodoForm.getByRole('textbox', {
+      name: 'Something to do...',
+    });
+    await expect(addTodoFormInput).toBeVisible();
+    await addTodoFormInput.fill(addedTodoLabel);
+    await addTodoForm.dispatchEvent('submit');
+    const addedTodoCard = list.getByRole('listitem', { name: addedTodoLabel });
+    await expect(addedTodoCard).toBeVisible();
+    const addedTodoCardCheckbox = addedTodoCard.getByRole('checkbox', {
+      name: addedTodoLabel,
+    });
+    await expect(addedTodoCardCheckbox).toBeChecked({ checked: false });
+
+    // to be used in further testing
+    const listitems = await list
+      .getByRole('listitem')
+      .filter({ hasNotText: addedTodoLabel })
+      .all();
+    // const pickedListItems = faker.helpers.arrayElements(listitems, 3);
+    const pickedListItems = listitems;
+
+    // TOGGLING A TODO
+    const todoToToggle = pickedListItems[0];
+    const todoToToggleId = await todoToToggle.getAttribute('id');
+    const todoToToggleCheckbox = todoToToggle.getByRole('checkbox');
+    const todoToToggleInitiallyChecked = await todoToToggleCheckbox.isChecked();
+    const todoToToggleExpectedChecked = !todoToToggleInitiallyChecked;
+    await todoToToggleCheckbox.click(); // TODO: "change" event
+    await expect(todoToToggleCheckbox).toBeChecked({
+      checked: todoToToggleExpectedChecked,
+    });
+
+    // EDITING A TODO
+    const todoToEdit = pickedListItems[1];
+    const todoToEditId = await todoToEdit.getAttribute('id');
+    const editButton = todoToEdit.getByRole('button', { name: 'Edit' });
+    await expect(editButton).toBeVisible();
+    await editButton.click();
+    const editForm = todoToEdit.getByRole('form', { name: 'Edit Todo' });
+    await expect(editForm).toBeVisible();
+    const editFormInput = editForm.getByRole('textbox', {
+      name: 'Something to do...',
+    });
+    await expect(editFormInput).toBeVisible();
+    const editedTodoLabel = 'E2E edited Todo label.';
+    await editFormInput.fill(editedTodoLabel);
+    await editForm.dispatchEvent('submit');
+    const editedTodo = list.getByRole('listitem', { name: editedTodoLabel });
+    await expect(editedTodo).toBeVisible();
+    // asserting `editedTodo` and `todoToEdit` are the same element
+    expect(await editedTodo.getAttribute('id')).toEqual(todoToEditId);
+
+    // DELETING A TODO
+    const todoToDelete = pickedListItems[2];
+    const todoToDeleteId = await todoToDelete.getAttribute('id');
+    const deleteButton = todoToEdit.getByRole('button', { name: 'Delete' });
+    await expect(deleteButton).toBeVisible();
+    await deleteButton.click();
+    const deleteDialog = page.getByRole('dialog', {
+      name: 'Delete this Todo?',
+    });
+    await expect(deleteDialog).toBeVisible();
+    await deleteDialog.getByRole('button', { name: 'Yes' }).click();
+
+    // BACKEND DATA POST-VERIFICATION
+    const res2 = await request.fetch(getTodoUrl, {
+      headers: {
+        Authorization: `Bearer ${aliceUserId}`,
+      },
+    });
+    const controlTodos2 = await res2.json();
+    // verifying updated Todo array
+    const controlIdHash2 = arrayIdHash(controlTodos2);
+    expect(controlIdHash2).not.toEqual(controlIdHash);
+    await expect(list).toHaveAttribute('data-idhash', controlIdHash2);
+    // verifying added Todo exists
+    const controlAddedTodoIndex = controlTodos2.findIndex(
+      (x: any) => x.label === addedTodoLabel,
+    );
+    expect(controlAddedTodoIndex).not.toEqual(-1);
+    // verifying toggled Todo's "done" state is updated
+    const controlTodoToToggle = controlTodos2.find(
+      (x: any) => x.id === todoToToggleId,
+    );
+    expect(controlTodoToToggle.done).toEqual(todoToToggleExpectedChecked);
+    // verifying edited Todo has the new label
+    const controlEditedTodo = controlTodos2.find(
+      (x: any) => x.id === todoToEditId,
+    );
+    expect(controlEditedTodo.label).toEqual(editedTodoLabel);
+  });
 });
