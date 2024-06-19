e9b8a354b431a238c862ee230bb3d145a397ae53
Anton <antoncodes@outlook.com>
2024-06-19 14:48:06 +0300

    TodoCard checkbox toggling int test

--- app/Todo/components/Card/Card.test.tsx
+++ app/Todo/components/Card/Card.test.tsx
@@ -3,9 +3,50 @@ import { render, screen, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { describe, expect, test } from 'vitest';
 import TodoCard from './Card';
-import { initTodo } from '~/Todo/types/Item';
+import TodoItem, { initTodo } from '~/Todo/types/Item';
 import { json } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
+import { faker } from '@faker-js/faker';
+
+// utility
+class ExecutionDetainer {
+  private frame = 100; // miliseconds
+  private maxDetentionTime = 5000; // miliseconds
+  private isDetaining = false;
+
+  private static delay = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+  async detainUntilResuming() {
+    this.isDetaining = true;
+    for (let i = 0; i < this.maxDetentionTime; i += this.frame) {
+      await ExecutionDetainer.delay(this.frame);
+      if (!this.isDetaining) {
+        break;
+      }
+    }
+  }
+
+  async resumeDetained() {
+    this.isDetaining = false;
+    await ExecutionDetainer.delay(this.frame);
+  }
+}
+
+// local utility
+function renderTodoCard(mockTodo: TodoItem, actions: any[] = []) {
+  const RemixStub = createRemixStub([
+    {
+      path: '/',
+      loader: () => json({ todo: mockTodo }),
+      Component: function () {
+        const { todo } = useLoaderData<typeof this.loader>();
+        return <TodoCard todo={todo} />;
+      },
+    },
+    ...actions,
+  ]);
+  render(<RemixStub />);
+}
 
 //
 // high level integration test
@@ -13,17 +54,7 @@ import { useLoaderData } from '@remix-run/react';
 describe('TodoCard', () => {
   test('data displaying', async () => {
     const mockTodo = initTodo({});
-    const RemixStub = createRemixStub([
-      {
-        path: '/',
-        loader: () => json({ todo: mockTodo }),
-        Component: function () {
-          const { todo } = useLoaderData<typeof this.loader>();
-          return <TodoCard todo={todo} />;
-        },
-      },
-    ]);
-    render(<RemixStub />);
+    renderTodoCard(mockTodo);
     const card = await screen.findByRole('listitem', { name: mockTodo.label });
     expect(card.getAttribute('id')).toBe(mockTodo.id);
     const checkbox = within(card).getByRole<HTMLInputElement>('checkbox', {
@@ -35,17 +66,7 @@ describe('TodoCard', () => {
   test('edit mode data displaying', async () => {
     const user = userEvent.setup();
     const mockTodo = initTodo({});
-    const RemixStub = createRemixStub([
-      {
-        path: '/',
-        loader: () => json({ todo: mockTodo }),
-        Component: function () {
-          const { todo } = useLoaderData<typeof this.loader>();
-          return <TodoCard todo={todo} />;
-        },
-      },
-    ]);
-    render(<RemixStub />);
+    renderTodoCard(mockTodo);
     const card = await screen.findByRole('listitem', { name: mockTodo.label });
     const editButton = within(card).getByRole('button', { name: 'Edit' });
     await user.click(editButton);
@@ -58,5 +79,29 @@ describe('TodoCard', () => {
       },
     );
     expect(checkbox.checked).toBe(mockTodo.done);
+  });
+
+  test('checkbox toggling', async () => {
+    const user = userEvent.setup();
+    const mockTodo = initTodo({
+      id: faker.string.uuid(),
+    });
+    const detainer = new ExecutionDetainer();
+    renderTodoCard(mockTodo, [
+      {
+        path: '/update/:id',
+        action: async function () {
+          await detainer.detainUntilResuming();
+          return json({});
+        },
+      },
+    ]);
+    const card = await screen.findByRole('listitem', { name: mockTodo.label });
+    const checkbox = within(card).getByRole<HTMLInputElement>('checkbox', {
+      name: 'Done',
+    });
+    await user.click(checkbox);
+    expect(card.ariaDisabled).toBe('true');
+    await detainer.resumeDetained();
   });
 });
