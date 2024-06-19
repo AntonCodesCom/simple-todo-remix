ca56d9a67812f47287cbcaa907524c88813035e6
Anton <antoncodes@outlook.com>
2024-06-19 11:21:02 +0300

    TodoCard edit mode data displaying int test init

--- app/Todo/components/Card/Card.test.tsx
+++ app/Todo/components/Card/Card.test.tsx
@@ -1,39 +1,56 @@
 import { createRemixStub } from '@remix-run/testing';
 import { render, screen, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { expect, test, vi } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import TodoCard from './Card';
 import { initTodo } from '~/Todo/types/Item';
 import { json } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 
 //
-// high-level integration test
+// high level integration test
 //
-test('TodoCard', async () => {
-  const mockTodo = initTodo({});
-  const RemixStub = createRemixStub([
-    {
-      path: '/',
-      loader: () => json({ todo: mockTodo }),
-      Component: function () {
-        const { todo } = useLoaderData<typeof this.loader>();
-        return <TodoCard todo={todo} />;
+describe('TodoCard', () => {
+  test('data displaying', async () => {
+    const mockTodo = initTodo({});
+    const RemixStub = createRemixStub([
+      {
+        path: '/',
+        loader: () => json({ todo: mockTodo }),
+        Component: function () {
+          const { todo } = useLoaderData<typeof this.loader>();
+          return <TodoCard todo={todo} />;
+        },
       },
-    },
-  ]);
-  const user = userEvent.setup();
-  render(<RemixStub />);
-  const card = await screen.findByRole('listitem', { name: mockTodo.label });
-  expect(card.getAttribute('id')).toBe(mockTodo.id);
-  const checkbox = within(card).getByRole<HTMLInputElement>('checkbox', {
-    name: 'Done',
+    ]);
+    render(<RemixStub />);
+    const card = await screen.findByRole('listitem', { name: mockTodo.label });
+    expect(card.getAttribute('id')).toBe(mockTodo.id);
+    const checkbox = within(card).getByRole<HTMLInputElement>('checkbox', {
+      name: 'Done',
+    });
+    expect(checkbox.checked).toBe(mockTodo.done);
   });
-  expect(checkbox.checked).toBe(mockTodo.done);
-  const editButton = within(card).getByRole('button', { name: 'Edit' });
-  await user.click(editButton);
-  screen.getByRole('form', { name: 'Edit Todo' });
-  const card2 = screen.getByRole('listitem', { name: mockTodo.label });
-  expect(card2.getAttribute('id')).toBe(mockTodo.id);
-  // TODO: "Done" checkbox value
+
+  test('edit mode data displaying', async () => {
+    const user = userEvent.setup();
+    const mockTodo = initTodo({});
+    const RemixStub = createRemixStub([
+      {
+        path: '/',
+        loader: () => json({ todo: mockTodo }),
+        Component: function () {
+          const { todo } = useLoaderData<typeof this.loader>();
+          return <TodoCard todo={todo} />;
+        },
+      },
+    ]);
+    render(<RemixStub />);
+    const card = await screen.findByRole('listitem', { name: mockTodo.label });
+    const editButton = within(card).getByRole('button', { name: 'Edit' });
+    await user.click(editButton);
+    const cardEditMode = screen.getByRole('listitem', { name: mockTodo.label });
+    expect(cardEditMode.getAttribute('id')).toBe(mockTodo.id);
+    // TODO: "Done" checkbox value
+  });
 });
