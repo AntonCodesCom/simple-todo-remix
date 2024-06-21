c7e470fa1860c24958d598af90b3751fd6bd47d7
Anton <antoncodes@outlook.com>
2024-06-21 10:12:33 +0300

    auth layout redirecting to main on authenticated user

--- app/routes/_auth.tsx
+++ app/routes/_auth.tsx
@@ -1,19 +1,30 @@
-import { LoaderFunctionArgs, json } from '@remix-run/node';
-import { Outlet, useLoaderData } from '@remix-run/react';
+import { LoaderFunctionArgs, redirect } from '@remix-run/node';
+import { Outlet } from '@remix-run/react';
 import CommonLayout from '~/Common/components/Layout';
 import config from '~/config';
 import sessions from '~/sessions';
 import fetchMe from '~/Auth/utils/fetchMe';
+import { UnauthorizedException } from '~/Auth/exceptions';
 
 // loader
 export async function loader({ request }: LoaderFunctionArgs) {
-  const { apiBaseUrl } = config();
   const { getSession, sessionCookieName } = sessions();
   const session = await getSession(request.headers.get('Cookie'));
   const accessToken = session.get(sessionCookieName);
-  const { username } = await fetchMe(accessToken, apiBaseUrl);
-  return json({ username });
-  // TODO: redirect to '/' if user is already authenticated
+  if (!accessToken) {
+    return null;
+  }
+  const { apiBaseUrl } = config();
+  try {
+    await fetchMe(accessToken, apiBaseUrl);
+    return redirect('/');
+  } catch (err) {
+    if (err instanceof UnauthorizedException) {
+      return null;
+    } else {
+      throw err;
+    }
+  }
 }
 
 // TODO: error boundary
@@ -22,7 +33,6 @@ export async function loader({ request }: LoaderFunctionArgs) {
  * Auth route layout component.
  */
 export default function LayoutAuth() {
-  const { username } = useLoaderData<typeof loader>();
   return (
     <CommonLayout>
       <Outlet />
