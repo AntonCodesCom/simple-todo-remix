f510d1d81ea9cab9731335855af94cb57bf885b7
Anton <antoncodes@outlook.com>
2024-06-21 09:58:20 +0300

    logout init

--- app/routes/_auth.login.tsx
+++ app/routes/_auth.login.tsx
@@ -1,11 +1,4 @@
-import {
-  Box,
-  Button,
-  Container,
-  TextField,
-  Typography,
-  useTheme,
-} from '@mui/material';
+import { Box, Button, Container, TextField, Typography } from '@mui/material';
 import { ActionFunctionArgs, redirect } from '@remix-run/node';
 import { Form } from '@remix-run/react';
 import config from '~/config';
