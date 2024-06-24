6b1fc826ab79efac838e1d1c72cea2815f15ae3a
Anton <antoncodes@outlook.com>
2024-06-24 10:11:51 +0300

    signup backend request

--- app/routes/_auth.signup.tsx
+++ app/routes/_auth.signup.tsx
@@ -1,13 +1,70 @@
 import { Box, Button, Container, TextField, Typography } from '@mui/material';
 import { ActionFunctionArgs, redirect } from '@remix-run/node';
 import { Form } from '@remix-run/react';
-import { UnauthorizedException } from '~/Auth/exceptions';
+import { ConflictException, UnauthorizedException } from '~/Auth/exceptions';
 import AuthLoggedInSchema, {
   authLoggedInSchema,
 } from '~/Auth/types/LoggedInSchema';
+import { authSignupSchema } from '~/Auth/types/SignupSchema';
 import config from '~/config';
 import sessions from '~/sessions';
 
+// utility
+async function fetchSignup(
+  username: string,
+  password: string,
+  apiBaseUrl: string,
+): Promise<AuthLoggedInSchema> {
+  const body = JSON.stringify({ username, password });
+  const url = new URL('auth/signup', apiBaseUrl);
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body,
+  });
+  if (!res.ok) {
+    if (res.status === 409) {
+      throw new ConflictException();
+    }
+    throw new Error('HTTP error occurred while fetching `POST /auth/login`.');
+  }
+  const data = await res.json();
+  return authLoggedInSchema.parse(data);
+}
+
+// action
+export async function action({ request }: ActionFunctionArgs) {
+  const form = await request.formData();
+  const data = authSignupSchema.parse({
+    username: form.get('username'),
+    password: form.get('password'),
+  });
+  const { username, password } = data;
+  const { apiBaseUrl } = config();
+  try {
+    const { accessToken } = await fetchSignup(username, password, apiBaseUrl);
+    const { getSession, commitSession, sessionCookieName } = sessions();
+    const session = await getSession(request.headers.get('Cookie'));
+    session.set(sessionCookieName, accessToken);
+    return redirect('/', {
+      headers: {
+        'Set-Cookie': await commitSession(session),
+      },
+    });
+  } catch (err) {
+    if (err instanceof UnauthorizedException) {
+      // TODO: error flash message
+      return redirect('.');
+    }
+    throw err;
+  }
+}
+
+/**
+ * Signup route component.
+ */
 export default function RouteSignup() {
   const headingHtmlId = 'RouteSignup_h1';
   return (
