1f65612813db8f18dd884275d062bd55acbf5767
Anton <antoncodes@outlook.com>
2024-06-25 17:59:13 +0300

    auto-logout for sessions that are about to expire

--- app/routes/_main.tsx
+++ app/routes/_main.tsx
@@ -1,16 +1,33 @@
 import { LoaderFunctionArgs, json, redirect } from '@remix-run/node';
 import { Outlet, useLoaderData } from '@remix-run/react';
+import { jwtDecode } from 'jwt-decode';
 import { UnauthorizedException } from '~/Auth/exceptions';
 import fetchMe from '~/Auth/utils/fetchMe';
 import CommonLayout from '~/Common/components/Layout';
 import config from '~/config';
 import sessions from '~/sessions';
 
+// utility
+function getAccessTokenExp(accessToken: string): number | undefined {
+  const decoded = jwtDecode(accessToken);
+  return decoded.exp;
+}
+
 // loader
 export async function loader({ request }: LoaderFunctionArgs) {
-  const { getSession, sessionCookieName } = sessions();
+  const { getSession, destroySession, sessionCookieName } = sessions();
   const session = await getSession(request.headers.get('Cookie'));
   const accessToken = session.get(sessionCookieName);
+  const exp = getAccessTokenExp(accessToken);
+  if (!exp || exp - Date.now() / 1000 < 86400) {
+    // if access token will expire in less than 1 day
+    // TODO: automatic session closing info alert
+    return redirect('/login', {
+      headers: {
+        'Set-Cookie': await destroySession(session),
+      },
+    });
+  }
   if (!accessToken) {
     return redirect('/login');
   }
@@ -20,7 +37,11 @@ export async function loader({ request }: LoaderFunctionArgs) {
     return json({ username });
   } catch (err) {
     if (err instanceof UnauthorizedException) {
-      return redirect('/login');
+      return redirect('/login', {
+        headers: {
+          'Set-Cookie': await destroySession(session),
+        },
+      });
     } else {
       throw err;
     }
