716b2a80f8abc74bea8b3fafbf54044ac6cafa0b
Anton <antoncodes@outlook.com>
2024-06-27 17:44:51 +0300

    LayoutMain loader int test init

---
+++ app/routes/_main/route.spec.tsx
@@ -0,0 +1,9 @@
+import { describe, test } from 'vitest';
+
+//
+// integration test
+//
+describe('LayoutMain loader', () => {
+  test.todo('access token verification');
+  test.todo('skipping access token revalidation');
+});
