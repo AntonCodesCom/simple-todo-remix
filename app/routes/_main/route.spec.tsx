d6391e15e8407b8d1e5faf2a81299768fed56aee
Anton <antoncodes@outlook.com>
2024-06-28 11:06:41 +0300

    LayoutMain loader unit test init

--- app/routes/_main/route.spec.tsx
+++ app/routes/_main/route.spec.tsx
@@ -1,9 +1,46 @@
-import { describe, test } from 'vitest';
+import { describe, expect, test, vi } from 'vitest';
+import mockJWT from '~/Testing/utils/mockJWT';
+import { loader } from './route';
+
+// mocking session
+const mockAccessToken = mockJWT();
+vi.mock('~/sessions', async (importOriginal) => {
+  return {
+    ...(await importOriginal<typeof import('~/sessions')>()),
+    authSession: () => ({
+      getAuthSession: async () => ({
+        get: () => mockAccessToken,
+      }),
+      commitAuthSession: async () => {},
+      destroyAuthSession: async () => {},
+      authSessionName: 'mockAuthSession',
+    }),
+  };
+});
+
+// mocking fetchMe
+const mockFetchMeFn = vi.fn();
+vi.mock('~/Auth/utils/fetchMe', () => ({
+  default: (...args: any) => {
+    return mockFetchMeFn(...args);
+  },
+}));
 
 //
-// integration test
+// unit test
 //
 describe('LayoutMain loader', () => {
-  test.todo('access token verification');
+  test('access token verification', async () => {
+    const mockLoaderFunctionArgs = {
+      request: {
+        headers: {
+          get: () => 'mockHeader',
+        },
+      },
+    } as any;
+    await loader(mockLoaderFunctionArgs); // act
+    expect(mockFetchMeFn).toHaveBeenCalledWith(mockAccessToken);
+  });
+
   test.todo('skipping access token revalidation');
 });
