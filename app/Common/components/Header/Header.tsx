03021a637a1ccadc5c43ab727e3a40ec05669703
Anton <antoncodes@outlook.com>
2024-06-29 22:27:11 +0300

    header elements nowrap

--- app/Common/components/Header/Header.tsx
+++ app/Common/components/Header/Header.tsx
@@ -1,4 +1,11 @@
-import { AppBar, Button, Container, Toolbar, Typography } from '@mui/material';
+import {
+  AppBar,
+  Button,
+  Container,
+  Stack,
+  Toolbar,
+  Typography,
+} from '@mui/material';
 import { Link } from '@remix-run/react';
 
 interface Props {
@@ -9,23 +16,35 @@ export default function CommonHeader({ username }: Props) {
   return (
     <AppBar position="static">
       <Toolbar component={Container}>
-        <Typography
-          variant="h6"
-          sx={{
-            flex: 1,
-            color: 'white',
-            textDecoration: 'none',
-          }}
-          component={Link}
-          to="/"
+        <Stack
+          alignItems="center"
+          justifyContent="space-between"
+          flex={1}
+          gap={1}
         >
-          Remix Todo
-        </Typography>
-        {username && (
-          <Button color="inherit" component={Link} to="/logout">
-            Logout ({username})
-          </Button>
-        )}
+          <Typography
+            variant="h6"
+            sx={{
+              color: 'white',
+              textDecoration: 'none',
+              whiteSpace: 'nowrap',
+            }}
+            component={Link}
+            to="/"
+          >
+            Remix Todo
+          </Typography>
+          {username && (
+            <Button
+              color="inherit"
+              component={Link}
+              to="/logout"
+              sx={{ whiteSpace: 'nowrap' }}
+            >
+              Logout ({username})
+            </Button>
+          )}
+        </Stack>
       </Toolbar>
     </AppBar>
   );
